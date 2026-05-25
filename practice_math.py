263186fce4bb392e64890d8ff355772b6d97a189
Alzetos <alzetos@yandex.ru>
2026-05-25 16:26:27 +0300

    Новые изменения в обработке данных

--- practice_math.py
+++ practice_math.py
@@ -1,21 +1,4 @@
-import numpy as np
+import numpy
 
-a = -16
-b = -1
-h = (b - a) / 9
-n_array = [3, 18, 56, 86, 69, 42, 18, 3, 1]
-
-
-def build_interval(a, h):
-    i = 1
-    array = [a]
-    while i != 10:
-        last = array[i - 1]
-        current = last + h
-        array.append(current)
-        last = current
-        i += 1
-    print(f"{array:.6f}")
-
-
-build_interval(a, h)
+array = numpy.zeros((2, 10))
+print(array)
